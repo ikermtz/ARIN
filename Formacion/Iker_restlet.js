483eba89eb2edc9f6526728184807b62023abfd4
ikermtz <49227870+ikermtz@users.noreply.github.com>
2023-03-23 12:28:34 +0100

    Add files via upload

--- Formacion/Iker_restlet.js
+++ Formacion/Iker_restlet.js
@@ -20,16 +20,14 @@
 	 
     function _post(payload) {
 
-        // Obtenemos los 5 campos de un movimiento
-        const parametrosAceptados = ["movimiento", "concepto", "importe", "fecha", "usuario"];
+        // Obtenemos los 5 campos de un movimiento 
+        const parametrosAceptados = ["movimiento", "concepto", "importe", "userId"];
         
         // Variables para cada campo
         var mov = payload.movimiento;
         var concepto = payload.concepto;
         var importe = payload.importe;
-        var fecha = payload.fecha;
-        var usuario = payload.usuario;
-        var user = payload.id;
+        var userId = payload.userId;
 
         for (param in payload){
             
@@ -41,31 +39,51 @@
             if(!payload.movimiento){
                 return n_error.create({name: 'ERROR', message: 'Se necesita el recibir el movimiento.'}).message;
             }
+        }
 
-            // Recibimos un movimiento con informacion correcta
-            if(mov != null){
+        // Mensaje que mostraremos en la petición de postman
+        var infoPostman;
 
-                // Existe el usuario?
-                if (!user){
-
-                    noExiste();
-                    n_log.debug({title: 'No existe ningun usuario con ese id.'});
-
-                // Existe el usuario    
-                } else {
+        // Recibimos un movimiento con informacion correcta
+        if(mov != null){
                     
-                    existe(mov, concepto, importe, fecha, user, usuario);
-                    n_log.debug({title: 'Existe un usuario con ese id.'});
+            // Existe usuario con ese id
+            if (!busqueda(userId)){
+
+                n_log.debug({title: 'No existe ningun usuario con ese id.'});
+                noExiste(mov, concepto, importe);
+                infoPostman = 'Petición enviada correctamente. Se creara un usuario con el movimiento de bienvenida y con el movimiento de la petición.';
+
+            // Existe el usuario    
+            } else {
+
+                // Necesitamos ciertos datos para ver si tiene saldo suficiente
+                var user = n_record.load({type: 'customrecord_ai_tb_registroej1', id: userId});
+                var saldo = user.getValue({fieldId: 'custrecord_ai_registroej1_saldo'});
+
+                // Saldo insuficiente
+                if( (saldo < importe) & (mov ==2) ){
+
+                    n_log.debug({title: 'Existe un usuario con ese id pero no tiene saldo suficiente'});
+                    infoPostman =  n_error.create({name: 'ERROR', message: 'ERROR: Movimiento denegado por saldo insuficiente.'}).message;
+        
+                // Saldo suficiente
+                } else {
+                
+                    n_log.debug({title: 'Existe un usuario con ese id y tiene saldo suficiente'});
+                    existe(mov, concepto, importe, userId);
+                    infoPostman = 'Petición enviada correctamente. Se creara el movimiento y se actualizara el saldo de ese usuario.';
 
                 }
-
-            // Recibimos un movimiento sin informacion
-            } else {
-                return n_error.create({name: 'ERROR', message: 'Se necesita el recibir un movimiento no vacio.'}).message;
             }
-        
+            
+        // Recibimos un movimiento sin informacion
+        } else {
+            infoPostman = n_error.create({name: 'ERROR', message: 'Se necesita el recibir un movimiento no vacio.'}).message;
         }
-		return 'Has usado un Post';
+        
+        
+		return infoPostman;
     }
 	 
     function _delete() {
@@ -76,131 +94,150 @@
     // FUNCIONES AUXILIARES
 
     // Si el usuario no existe tenemos que crearlo y gestionar el movimiento de regalo
-    function noExiste(){
+    function noExiste(mov, concepto, importe){
         
-        crearUsuario();
+        //Creamos el nuevo usuario
+        var newUser = n_record.create({type: 'customrecord_ai_tb_registroej1', isDynamic: true});
 
-        return true;
-    }
+        // Actualizamos el saldo
+        var saldoAct;
 
-    function crearUsuario(){
-        
-        var newUser = n_record.create({type: 'customrecord_ai_tb_registroej1'});
+        // Ingreso
+        if( mov == 1 ){
 
-        newUser.setValue({fieldId: 'custrecord_ai_registroej1_saldo', value: 0});
-        newUser.setValue({fieldId: 'custrecord_ai_registroej1_pref', value: false});
+            saldoAct = 100 + importe;
 
-        // Guardamos el nuevo usuario
-        try {
-            newUser.save();
-            n_log.debug({title: 'Movimiento de regalo creado correctamente.'});
-        } catch (e) {
-            n_log.error({details: e.message});
-        }
-
-        var newId = newUser.getValue({fieldId: 'custrecord_ai_registroej1_id'});
-
-        primerMovimiento(newId);
-
-        return true;
-    }
-
-    function primerMovimiento(newId){
-        
-        n_log.debug({title: 'Vamos a crear el movimiento de regalo.'})
-        // Creamos un nuevo record de tipo movimiento
-        var newMov = n_record.create({type: 'customrecord_ai_movimientosej1_mov'});
-        var saldoRegalo = 100;
-
-        // Metemos los valores del movimiento de regalo
-        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_usuario', value: newId});
-        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_mov', value: 1});
-        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_concepto', value: "Regalo por crear una cuenta"});
-        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_fecha', value: fecha});
-        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_importe', value: saldoRegalo});
-        
-        // Guardamos el movimiento
-        try {
-            newMov.save();
-            n_log.debug({title: 'Movimiento de regalo creado correctamente.'});
-        } catch (e) {
-            n_log.error({details: e.message});
-        }
-
-        return true;
-    }
-
-    // Mirar si hay saldo suficiente
-    function existe(mov, concepto, importe, fecha, user, usuario){
-        
-        var saldo = usuario.getValue({fieldId: 'custrecord_ai_registroej1_saldo'});
-
-        // Saldo insuficiente
-        if( (saldo < importe) & (mov ==2) ){
-         
-            devolverError();
-
-        // Saldo suficiente
+        // Gasto
         } else {
-            
-            generarMovimiento(mov, concepto, importe, fecha, user, saldo, usuario);
+
+            saldoAct = 100 - importe;
 
         }
+
+        n_log.debug({title: 'Saldo 1', saldoAct});
+
+        newUser.setValue({fieldId: 'custrecord_ai_registroej1_saldo', value: saldoAct});
         
+        // Guardamos el usuario y sacamos el id
+        try {
+            var newId = newUser.save({enableSourcing: true, ignoreMandatoryFields: false});
+            n_log.debug({title: 'Movimiento de regalo creado correctamente.'});
+        } catch (e) {
+            n_log.error({details: e.message});
+        }
+
+        n_log.debug({title: 'Saldo 2', saldoAct});
+
+        n_log.debug({title: 'New id', details: newId});
+    
+        // Información movimiento regalo
+        var movRegalo = 1;
+        var conceptoRegalo = 'Regalo por crear una cuenta nueva';
+        var importeRegalo = 100;        
+
+        // Creamos el movimiento de regalo
+        generarMovimiento(movRegalo, conceptoRegalo, importeRegalo, newId);
+
+        // Creamos el movimiento de la petición
+        generarMovimiento(mov, concepto, importe, newId);
+
+        n_log.debug({title: 'Saldo 3', saldoAct});
+
         return true;
     }
 
-    function generarMovimiento(mov, concepto, importe, fecha, user, saldo, usuario){
+    // Tiene saldo suficiente
+    function existe(mov, concepto, importe, userId){
         
+        // Necesitamos ciertos datos para actualizar el saldo del usuario
+        var user = n_record.load({type: 'customrecord_ai_tb_registroej1', id: userId});
+        var saldo = user.getValue({fieldId: 'custrecord_ai_registroej1_saldo'});
+
         // Ingreso 
         if( mov == 1 ){
+
             saldo = saldo + importe;
 
         // Gasto
         } else {
+
             saldo = saldo - importe;
+
         }
 
-        // Actualizamos saldo
-        usuario.setValue({fieldId: 'custrecord_ai_registroej1_saldo', value: saldo});
+        // Creamos el movimiento de la petición
+        generarMovimiento(mov, concepto, importe, userId);
 
-        // Guardamos el saldo actualizado
+        // Actualizamos el saldo
+        user.setValue({fieldId: 'custrecord_ai_registroej1_saldo', value: saldo});
+        
+        // Guardamos el usuario con saldo actualizado
         try {
             user.save();
-            n_log.debug({title: 'Saldo actualizado correctamente.'});
         } catch (e) {
             n_log.error({details: e.message});
         }
 
-        // Creamos el movimiento
+        return true;
+    
+    }
+
+    function generarMovimiento(mov, concepto, importe, userId){
+        
         var newMov = n_record.create({type: 'customrecord_ai_movimientosej1_mov', isDynamic: true});
 
-        // Le metemos los valores al movimiento
-        newMov.setValue({fieldId: 'custrecord_ai_movimientosej1_mov', value: mov});
-        newMov.setValue({fieldId: 'custrecord_ai_movimientosej1_concepto', value: concepto});
-        newMov.setValue({fieldId: 'custrecord_ai_movimientosej1_importe', value: importe});
-        newMov.setValue({fieldId: 'custrecord_ai_movimientosej1_fecha', value: fecha});
-        newMov.setValue({fieldId: 'custrecord_ai_movimientosej1_usuario', value: user});
-
-        // Guardamos el movimiento creado
-        try { 
-            newMov.save();
-            n_log.debug({title: 'Nuevo movimiento creado.'});
-        } catch (e) {
-            log.error({title: e.name, details: e.message});
-        }
-        return true;
-    }
-
-    function devolverError(){
+        var fecha = new Date();
         
-        return n_error.create({name: 'ERROR', message: 'No se puede crear el movimiento ya que no hay saldo suficiente'}).message;
+        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_usuario', value: userId});
+        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_mov', value: mov});
+        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_concepto', value: concepto});
+        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_fecha', value: fecha});
+        newMov.setValue({fieldId:'custrecord_ai_movimientosej1_importe', value: importe});
 
+        try {
+            newMov.save();
+            n_log.debug({title: 'Movimiento creado correctamente.'});
+        } catch (e) {
+            n_log.error({details: e.message});
+        }
     }
+
+
+    function busqueda(userId){
+        
+        var existe = false;
+        var res = buscar(userId);
+        n_log.debug({details: res});
+        if (res != null){
+            existe = true;
+        }
+        return existe;
+        
+    }
+    
+    function buscar(userId){
+        var search = n_search.create({
+            type: 'customrecord_ai_tb_registroej1',
+            filters: ['internalId', n_search.Operator.IS, userId],
+            columns: [n_search.createColumn({name: 'internalId', label: 'id'})]
+        }).run().getRange({start: 0, end: 1});
+
+        for (var row in search) return search[row].id;
+        
+    }
+
+    /*Postman:
+        {
+            "movimiento": "2",
+            "concepto": "Bufanda real betis",
+            "importe": "1",
+            "userId": "1"
+        }
+    */
 
     return {
         get: _get,
         post: _post,
         delete: _delete
     };
- }); 
+}); 
